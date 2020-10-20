dc16115f04bb8c7f6843b16cfc3542cd29c3f8e9
nomuraxxx <dinomura@gmail.com>
2020-10-20 20:22:48 -0300

    code review

--- fibonacci.py
+++ fibonacci.py
@@ -4,22 +4,21 @@ from math import sqrt
 
 app = Flask(__name__)
 
-@app.route('/')
-def fibo_code():
-   proximo = 1
-   anterior = 0
-   limite = 50
-   found = 0
-   resposta = "0,"
-   while (found < limite):
-       tmp = proximo
-       proximo = proximo + anterior
-       anterior = tmp
-       found=found+1
-       resposta+= str(proximo) + ","
+nmax = 100
 
+    n1 = 0
+    n2 = 1
+    cont = 0
+    fib = 0
 
-   return resposta
+    fib = ("1000 primeiros numeros Fibonacci: <br> <br>")
+
+    while cont < nmax:
+        fib = n1 + n2
+        n1 = n2
+        n2 = fib
+        cont = cont + 1
+    return fib
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
